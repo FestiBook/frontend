3049c5afcfa625082c9254f725d20e370c73a75a
yura <yuraa0302@gmail.com>
2024-09-19 16:32:50 +0900

    design : 헤더 반응형 ui 제작

--- src/components/Layout/HeaderLayout/index.tsx
+++ src/components/Layout/HeaderLayout/index.tsx
@@ -1,9 +1,11 @@
-import { ReactNode, useState } from "react";
+import { useState } from "react";
 
 import * as S from "../../../styles/header.styled";
 
 function HeaderLayout() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
   const handleMouseEnter = () => {
     setIsDropdownOpen(true);
   };
@@ -12,20 +14,58 @@ function HeaderLayout() {
     setIsDropdownOpen(false);
   };
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <S.Header>
-      <S.Logo src="/icons/logo.svg" width={150} height={100} alt="로고" />
-      <S.Menu>
-        <S.Div href="/">• 홈</S.Div>
-        <S.Div href="/">• AI 플래너</S.Div>
-        <S.Div href="/">• 여행 가이드</S.Div>
-        <S.Div href="/login">• 로그인</S.Div>
-        <S.Img
-          src="/icons/search.svg"
-          width={23}
-          height={23}
-          alt="검색"
-        ></S.Img>
+      <S.Click href="/">
+        <S.Logo src="/icons/logo.svg" width={150} height={100} alt="로고" />
+      </S.Click>
+      <S.HamburgerMenuIcon
+        onClick={toggleMobileMenu}
+        src="/icons/hamburger.svg"
+        alt="햄버거 메뉴"
+        width={23}
+        height={23}
+      />
+
+      {/* 모바일 아이콘 */}
+      <S.IconWrapper>
+        <S.Img src="/icons/search.svg" width={23} height={23} alt="검색" />
+        <S.Profile
+          src="/icons/avatar.svg"
+          alt="프로필사진"
+          width={40}
+          height={40}
+          onClick={toggleDropdown}
+        />
+      </S.IconWrapper>
+
+      <S.MobileIcon>
+        <S.Dropdown isOpen={isDropdownOpen}>
+          <S.DropdownItem href="/mypage">마이페이지</S.DropdownItem>
+          <S.DropdownItem href="/404">로그아웃</S.DropdownItem>
+        </S.Dropdown>
+      </S.MobileIcon>
+
+      <S.DesktopIcons>
+        <S.Menu>
+          <S.Div href="/">• 홈</S.Div>
+          <S.Div href="/">• AI 플래너</S.Div>
+          <S.Div href="/">• 여행 가이드</S.Div>
+          <S.Div href="/login">• 로그인</S.Div>
+        </S.Menu>
+
+        <S.Click href="/search">
+          <S.Img src="/icons/search.svg" width={23} height={23} alt="검색" />
+        </S.Click>
+
         <S.MyInfo
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
@@ -44,7 +84,16 @@ function HeaderLayout() {
             </S.Dropdown>
           )}
         </S.MyInfo>
-      </S.Menu>
+      </S.DesktopIcons>
+
+      {isMobileMenuOpen && (
+        <S.MobileMenuDropdown>
+          <S.MobileMenuItem href="/">• 홈</S.MobileMenuItem>
+          <S.MobileMenuItem href="/">• AI 플래너</S.MobileMenuItem>
+          <S.MobileMenuItem href="/">• 여행 가이드</S.MobileMenuItem>
+          <S.MobileMenuItem href="/login">• 로그인</S.MobileMenuItem>
+        </S.MobileMenuDropdown>
+      )}
     </S.Header>
   );
 }
--- src/styles/header.styled.ts
+++ src/styles/header.styled.ts
@@ -4,7 +4,6 @@ import Link from "next/link";
 import styled from "@emotion/styled";
 
 import { MOBILE_MEDIA_QUERY } from "./const";
-import { TABLET_MEDIA_QUERY } from "./const";
 
 interface DropdownProps {
   isOpen: boolean;
@@ -13,28 +12,111 @@ interface DropdownProps {
 export const Header = styled.div`
   width: 100vw;
   height: 4.3rem;
-  flex-shrink: 0;
-  background-color: #000;
-  background: var(--neutral-white, #fff);
+  background-color: #fff;
   box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  padding: 0 1rem;
+  position: relative;
 
   @media ${MOBILE_MEDIA_QUERY} {
-    font-size: 10px;
+    justify-content: space-between;
   }
 `;
 
 export const Logo = styled(Image)`
   flex-shrink: 0;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
 `;
 
-export const Menu = styled.div`
+export const MobileMenuDropdown = styled.div`
+  position: absolute;
+  top: 4.3rem;
+  left: 0;
+  width: 100%;
+  background-color: white;
+  padding: 1rem 0;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 1000;
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+`;
+
+export const MobileMenuItem = styled.a`
+  padding: 1rem 1.5rem;
+  text-align: left;
+  color: black;
+  font-size: 1.1rem;
+  text-decoration: none;
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+export const Img = styled(Image)`
+  margin-left: 1rem;
+  cursor: pointer;
+`;
+
+export const MyInfo = styled.div`
+  display: flex;
+  justify-content: center;
   align-items: center;
+  cursor: pointer;
+  position: relative;
+`;
+
+export const Profile = styled(Image)`
+  cursor: pointer;
+  z-index: 10;
+`;
+
+export const Name = styled.span`
+  margin-left: 0.5rem;
+  color: var(--neutral-black, #000);
+  font-family: Montserrat;
+  font-size: 0.9375rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-right: 2rem;
+`;
+
+export const Dropdown = styled.div<DropdownProps>`
+  display: none;
+  position: absolute;
+  top: 100%;
+  right: 0;
+  background-color: white;
+  border-radius: 0.4em;
+  width: 150px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  z-index: 9999;
+  display: ${({ isOpen }) => (isOpen ? "flex" : "none")};
+  visibility: ${({ isOpen }) => (isOpen ? "visible" : "hidden")};
+  flex-direction: column;
+  transition: opacity 0.3s ease-in-out;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    top: 4.3rem;
+    right: 1rem;
+  }
+`;
+
+export const DropdownItem = styled.a`
+  padding: 10px;
+  text-align: center;
+  color: black;
+  text-decoration: none;
+  &:hover {
+    background-color: #f0f0f0;
+  }
 `;
 
 export const Div = styled(Link)`
@@ -47,65 +129,68 @@ export const Div = styled(Link)`
   font-weight: 400;
   line-height: normal;
   margin-right: 3rem;
+`;
 
-  @media ${TABLET_MEDIA_QUERY} {
-    font-size: 0.84rem;
-    margin-right: 3rem;
-  }
+export const IconWrapper = styled.div`
+  display: none;
 
   @media ${MOBILE_MEDIA_QUERY} {
-    font-size: 0.64rem;
-    margin-right: 1rem;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    position: absolute;
+    right: 1rem;
   }
 `;
 
-export const Img = styled(Image)`
+export const Menu = styled.div`
   display: flex;
-  margin-right: 3rem;
-`;
-
-export const MyInfo = styled.div`
-  display: flex;
-  justify-content: center;
   align-items: center;
-  cursor: pointer;
-  position: relative;
-`;
-export const Profile = styled(Image)``;
-export const Name = styled.div`
-  color: var(--neutral-black, #000);
-  font-family: Montserrat;
-  font-size: 0.9375rem;
-  font-style: normal;
-  font-weight: 600;
-  line-height: normal;
-  margin-left: 10px;
-  margin-right: 2rem;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    display: none;
+  }
 `;
 
-export const Dropdown = styled.div<DropdownProps>`
-  position: absolute;
-  top: 100%;
-  right: 0;
-  background-color: white;
-  margin-right: 2rem;
-  margin-top: 0.3rem;
-  border-radius: 0.4em;
-  width: 150px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-  z-index: 1000;
-  display: ${({ isOpen }) =>
-    isOpen ? "flex" : "none"}; /* 드롭다운이 열리고 닫히는 동작 */
-  flex-direction: column;
-  transition: opacity 0.3s ease-in-out;
+export const HamburgerMenuIcon = styled(Image)`
+  display: none;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    display: flex;
+    align-items: flex-start;
+    cursor: pointer;
+    position: absolute;
+    left: 1rem;
+  }
 `;
 
-export const DropdownItem = styled.a`
-  padding: 10px;
-  text-align: left;
-  color: black;
+export const DesktopIcons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  @media ${MOBILE_MEDIA_QUERY} {
+    display: none;
+  }
+`;
+
+export const Click = styled.a`
+  display: flex;
+  align-items: center;
   text-decoration: none;
-  &:hover {
-    background-color: #f0f0f0;
+  color: inherit;
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    display: flex;
+    padding: 10px;
+    text-align: center;
+    color: black;
+    text-decoration: none;
+    &:hover {
+      background-color: #f0f0f0;
+    }
   }
 `;
